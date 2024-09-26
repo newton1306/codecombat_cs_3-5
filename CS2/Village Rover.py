d9494662597de075c3bdab3f77419058ecd04970
nst.si <nst.si.khukhansc@gmail.com>
2024-09-26 20:36:58 +0700

    ssssssss

---
+++ CS2/Village Rover.py
@@ -0,0 +1,11 @@
+while True:
+    enemy = hero.findNearestEnemy()
+    distance = hero.distanceTo(enemy)
+    if distance < 10:
+        # Attack if they get too close to the peasant.
+        hero.attack(enemy)
+        pass
+    # Else, stay close to the peasant! Use else.
+    else:
+        hero.moveXY(39, 39)        
+    
