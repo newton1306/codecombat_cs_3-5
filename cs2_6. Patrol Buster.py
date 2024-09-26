3f392d988b08f6ba612292ec52232750720ad62c
nst.si <nst.si.khukhansc@gmail.com>
2024-09-26 20:18:55 +0700

    กกก
    หกด

---
+++ cs2_6. Patrol Buster.py
@@ -0,0 +1,19 @@
+# The hero is all confused!
+# Correct their path so they don't walk on the mines.
+
+hero.moveXY(11, 35)
+hero.moveXY(35, 25)
+hero.moveXY(40, 56)
+hero.moveXY(77, 58)
+# There is a headhunter in the area!
+# Move around the forest to avoid his line-of-sight.
+
+while True:
+    hero.moveXY(56,44)
+    hero.moveXY(40,56)
+    # Use moveXY to keep moving around the forest to survive.
+    # Don't forget about indents.
+    hero.moveXY(24,44)
+    hero.moveXY(24,24)
+    hero.moveXY(40,12)
+    hero.moveXY(56,24)
