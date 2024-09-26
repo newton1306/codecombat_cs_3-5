d6ab919816cf6c36515b92df1c90815f813b8511
nst.si <nst.si.khukhansc@gmail.com>
2024-09-26 21:20:41 +0700

    ก

--- CS2/The Agrippa Defense.py
+++ CS2/The Agrippa Defense.py
@@ -1,11 +1,14 @@
 while True:
     enemy = hero.findNearestEnemy()
-    distance = hero.distanceTo(enemy)
-    if distance < 10:
-        # Attack if they get too close to the peasant.
-        hero.attack(enemy)
-        pass
-    # Else, stay close to the peasant! Use else.
-    else:
-        hero.moveXY(39, 39)        
-    
+    if enemy:
+        # Replace this with your own code.
+        # Find the distance to the enemy with distanceTo.
+        s = hero.distanceTo(enemy)
+        # If the distance is less than 5 meters...
+        if s<5:
+            # ... if "cleave" is ready, cleave!
+            if (hero.isReady("cleave")):
+                hero.cleave(enemy)
+            # ... else, just attack.
+            else:
+                hero.attack(enemy)
